ab619ec9016bf4d1b31fd49bec879f6688f18c37
Nguyen The Manh <hoangtupro.a4@gmail.com>
2021-11-26 07:50:23 +0700

    add backer

--- js/data.js
+++ js/data.js
@@ -1,7 +1,18 @@
 const data = {
     roadmap: {
         logo: 'logoshiba.svg'
-    }
+    },
+    backer: [
+        'seaour.png',
+        'seaour2.png',
+    ]
 };
 var img = '<img src="img/'+data.roadmap.logo+'">';
-$('.timeline-content-icon').html(img)
+$('.timeline-content-icon').html(img);
+for(var i = 0; i < data.backer.length; i ++) {
+    var backer = $('.backer-item').clone();
+    backer.removeAttr('style');
+    backer.find('img').attr('src', 'img/' + data.backer[i]);
+    $('.backer-list').append(backer);
+}
+$('.backer-list .backer-item:last').remove();
