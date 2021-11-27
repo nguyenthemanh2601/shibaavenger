06286412842c22e89de8f55549e6d590aaaa25bf
Nguyen The Manh <hoangtupro.a4@gmail.com>
2021-11-27 15:35:50 +0700

    fix backer

--- js/data.js
+++ js/data.js
@@ -5,14 +5,18 @@ const data = {
     backer: [
         'seaour.png',
         'seaour2.png',
+        'seaour2.png',
+        'seaour2.png',
     ]
 };
+
 var img = '<img src="img/'+data.roadmap.logo+'">';
 $('.timeline-content-icon').html(img);
+
 for(var i = 0; i < data.backer.length; i ++) {
-    var backer = $('.backer-item').clone();
+    console.log(i);
+    var backer = $('.backer-item:first').clone();
     backer.removeAttr('style');
     backer.find('img').attr('src', 'img/' + data.backer[i]);
     $('.backer-list').append(backer);
 }
-$('.backer-list .backer-item:last').remove();
